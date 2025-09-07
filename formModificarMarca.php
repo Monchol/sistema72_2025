6f1a51c9705bbd42639d72979c3c47b85cabf9d1
ImAlex14 <alexkapriela85@gmail.com>
2025-09-07 14:18:57 -0300

    Add files via upload

---
+++ formModificarMarca.php
@@ -0,0 +1,25 @@
+<?php
+require 'funciones/conexion.php';
+require 'funciones/marcas.php';
+
+$marca = verMarcaPorID();
+
+include 'includes/header.html';
+include 'includes/nav.php';
+
+?>
+<main class="container">
+    <h1>Modificar Marca</h1>
+
+    <form action="modificarMarca.php" method="POST">
+        ID:
+        <input type="text" name="idMarca" value="<?= $marca['idMarca'] ?>" required>
+        Marca:
+        <input type="text" name="mkNombre" value="<?= $marca['mkNombre'] ?>" required>
+        <input type="submit" name="" value="MODIFICAR">
+    </form>
+</main>
+
+<?php
+include 'includes/footer.php';
+?>
