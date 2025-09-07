cf41b8fadaccc4c5f987d78125c0123849727268
ImAlex14 <alexkapriela85@gmail.com>
2025-09-07 14:33:59 -0300

    Add files via upload

---
+++ modificarCategoria.php
@@ -0,0 +1,28 @@
+<?php
+require 'funciones/conexion.php';
+require 'funciones/categorias.php';
+
+$chequeo = modificarCategoria();
+
+include 'includes/header.html';
+include 'includes/nav.php';
+
+
+?>
+
+<main class="container">
+    <h1>Modificar Categoria</h1>
+    <?php
+    $class = 'danger';
+    $mensaje = 'No se puede modificar la categoria';
+
+    if ($chequeo) {
+        $class = 'success';
+        $mensaje = 'Categoria modificado correctamente';
+    }
+    ?>
+    <div class="alert alert-<?= $class ?>" <?= $mensaje ?>></div>
+</main>
+<?php
+include 'includes/footer.php';
+?>
---
+++ modificarMarca.php
@@ -0,0 +1,28 @@
+<?php
+require 'funciones/conexion.php';
+require 'funciones/marcas.php';
+
+$chequeo = modificarMarca();
+
+include 'includes/header.html';
+include 'includes/nav.php';
+
+
+?>
+
+<main class="container">
+    <h1>Modificar Marca</h1>
+    <?php
+    $class = 'danger';
+    $mensaje = 'No se puede modificar la marca';
+
+    if ($chequeo) {
+        $class = 'success';
+        $mensaje = 'Marca modificado correctamente';
+    }
+    ?>
+    <div class="alert alert-<?= $class ?>" <?= $mensaje ?>></div>
+</main>
+<?php
+include 'includes/footer.php';
+?>
